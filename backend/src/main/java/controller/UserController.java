86afe4f30e14f5ce88d1196eb25f9725eba95c79
allysson13 <assuncaoallyssonbruno@gmail.com>
2023-07-10 14:28:46 -0300

    autowiring

--- backend/src/main/java/controller/UserController.java
+++ backend/src/main/java/controller/UserController.java
@@ -1,7 +1,13 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import repository.UserRepository;
+
+@RestController
 public class UserController {
 
-
+    @Autowired
+    UserRepository userRepository;
 
 }
